45ecb9e1c47230bdb38b2940a8fab06e1a03ec83
Xeophon <46377542+xeophon@users.noreply.github.com>
2026-08-01 00:29:30 +0200

    wip(fix9104): leftover in-worktree state at interruption

--- dbg2.mjs
+++
@@ -1,4 +0,0 @@
-
-import { attackList, pmOf } from './js/mhitm.js';
-const mon = { data: { name: 'orc-captain', mlevel: 5, hpLevel: 6 } };
-console.log(JSON.stringify(pmOf(mon)?.name), JSON.stringify(attackList(mon)));
